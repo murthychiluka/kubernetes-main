eabffd21d34530ab4fed8aac7d310019c56124a0
murthychiluka <100435093+murthychiluka@users.noreply.github.com>
2026-09-01 23:08:58 +0530

    Modify test.sh for new URL and CPU-load pod instructions
    Updated the curl command to use a new URL and added instructions for creating a CPU-load pod in Kubernetes.

--- day-4-horizonatl-scaling/test.sh
+++ day-4-horizonatl-scaling/test.sh
@@ -1,6 +1,19 @@
 for j in {1..10}; do  
     for i in {1..1000}; do  
-        curl -s -o /dev/null -w "%{http_code}\n" http://a5519ee8ee62147a7b7c055673f1ee70-2108986447.us-east-1.elb.amazonaws.com/ &  
+        curl -s -o /dev/null -w "%{http_code}\n" http://a4e243c5e0c19421ebb17e5b2231df50-1741334213.us-east-1.elb.amazonaws.com &  
     done  
     wait  # Wait for all background curl processes to finish before next iteration
 done
+
+```text
+You can create a temporary CPU-load pod:
+
+kubectl run cpu-load \
+  --image=busybox \
+  --restart=Never \
+  -- /bin/sh -c "while true; do :; done"
+
+Then watch:
+
+kubectl get hpa -w
+```
